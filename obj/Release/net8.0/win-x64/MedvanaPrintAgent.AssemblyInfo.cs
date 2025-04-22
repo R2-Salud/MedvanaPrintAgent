78941b4c86e9eb6eff6859b0b8bd39963edb8fea
rmiranda-r2 <rmiranda@r2salud.com>
2025-04-22 12:57:56 -0600

    New compile

--- obj/Release/net8.0/win-x64/MedvanaPrintAgent.AssemblyInfo.cs
+++ obj/Release/net8.0/win-x64/MedvanaPrintAgent.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MedvanaPrintAgent")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1d0f350f528ffe7b32002ce9ec9524d1ddf4ec4b")]
 [assembly: System.Reflection.AssemblyProductAttribute("MedvanaPrintAgent")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MedvanaPrintAgent")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
